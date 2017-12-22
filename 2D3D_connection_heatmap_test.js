e9f3eb7b57a85c6896ac34f69610d99fa1799f01
ray38 <xlei38@gatech.edu>
2017-12-21 23:17:26 -0500

    updated pointcloud update function

--- 2D3D_connection_heatmap_test.js
+++ 2D3D_connection_heatmap_test.js
@@ -1132,6 +1132,7 @@ function getPointCloudGeometry(view) {
 	var colors = new Float32Array(count * 3);
 	var sizes = new Float32Array(count);
 	var alphas = new Float32Array(count);
+	var parentBlock = new Float32Array(count);
 
 	var colorMap = options.colorMap;
 	var numberOfColors = 512;
@@ -1179,6 +1180,8 @@ function getPointCloudGeometry(view) {
 					sizes[i] = 0;
 				}
 
+				parentBlock[i] = k;
+
 				i++;
 				i3 += 3;
 			}
@@ -1189,32 +1192,98 @@ function getPointCloudGeometry(view) {
 	geometry.addAttribute('customColor', new THREE.BufferAttribute(colors, 3));
 	geometry.addAttribute('size', new THREE.BufferAttribute(sizes, 1));
 	geometry.addAttribute('alpha', new THREE.BufferAttribute(alphas, 1));
+	geometry.parentBlockMap = parentBlock;
 
 	var System = new THREE.Points(geometry, shaderMaterial);
 	view.System = System;
 	scene.add(System);
 }
 
+/*export function updatePointCloudGeometry(view){
+
+var options = view.options;
+
+var particles = options.pointCloudParticles;
+var num_blocks = view.data.length;
+var points_in_block = new Float32Array(num_blocks);
+var total = 100;
+var count = 0;
+
+for ( var k = 0; k < num_blocks; k ++) {
+	//var num_points  = Math.floor((unfilteredData[k]['n'] / total) * particles);
+	var num_points  = Math.min(Math.floor((view.data[k][options.density] / total) * particles),40);
+	points_in_block[k] = num_points;
+	count += num_points;
+}
+
+
+var colors = new Float32Array(count *3);
+var sizes = new Float32Array( count);
+var alphas = new Float32Array( count);
+
+var colorMap = options.colorMap;
+var numberOfColors = 512;
+
+var lut = new THREE.Lut( colorMap, numberOfColors );
+lut.setMax( options.pointCloudColorSettingMax );
+lut.setMin( options.pointCloudColorSettingMin );
+view.lut = lut;
+
+var i = 0, i3 = 0;
+var temp_num_points = 0;
+for ( var k = 0; k < num_blocks; k ++) {
+	temp_num_points  =  points_in_block[k];
+	if (temp_num_points > 0){
+
+		var x_start = view.data[k]['xPlot'];
+		var y_start = view.data[k]['yPlot'];
+		var z_start = view.data[k]['zPlot'];
+		var x_end = x_start + 1;
+		var y_end = y_start + 1;
+		var z_end = z_start + 1;
+		
+		for (var j = 0; j < temp_num_points; j ++){
+
+			var color = lut.getColor( view.data[k][options.propertyOfInterest] );
+			
+			colors[ i3 + 0 ] = color.r;
+			colors[ i3 + 1 ] = color.g;
+			colors[ i3 + 2 ] = color.b;
+			
+			if (	(x_start >= options.x_low) 	&& (x_end <= options.x_high) 	&&
+				(y_start >= options.y_low) 	&& (y_end <= options.y_high)	&&
+				(z_start >= options.z_low) 	&& (z_end <= options.z_high)	&& view.data[k].selected)
+				{
+					alphas[ i ] = options.pointCloudAlpha;
+					sizes[ i ] = options.pointCloudSize;
+				}
+			else {
+				alphas[ i ] = 0;
+				sizes[ i ] = 0;
+			}
+
+			
+			i++;
+			i3+=3;
+		}
+	}			
+}
+
+view.System.geometry.addAttribute( 'customColor', new THREE.BufferAttribute( colors, 3 ) );
+view.System.geometry.addAttribute( 'size', new THREE.BufferAttribute( sizes, 1 ) );
+view.System.geometry.addAttribute( 'alpha', new THREE.BufferAttribute( alphas, 1 ) );
+}*/
+
 function updatePointCloudGeometry(view) {
 
 	var options = view.options;
+	var positionArray = view.System.geometry.attributes.position.array;
+	var parentBlock = view.System.geometry.parentBlockMap;
 
 	var particles = options.pointCloudParticles;
 	var num_blocks = view.data.length;
 	var points_in_block = new Float32Array(num_blocks);
-	var total = 100;
-	var count = 0;
-
-	for (var k = 0; k < num_blocks; k++) {
-		//var num_points  = Math.floor((unfilteredData[k]['n'] / total) * particles);
-		var num_points = Math.min(Math.floor(view.data[k][options.density] / total * particles), 40);
-		points_in_block[k] = num_points;
-		count += num_points;
-	}
-
-	var n = 100;
-	var n2 = Math.pow(n, 2);
-	var n_inc = n / 2;
+	var count = view.System.geometry.attributes.size.array.length;
 
 	var colors = new Float32Array(count * 3);
 	var sizes = new Float32Array(count);
@@ -1228,40 +1297,26 @@ function updatePointCloudGeometry(view) {
 	lut.setMin(options.pointCloudColorSettingMin);
 	view.lut = lut;
 
-	var i = 0,
-	    i3 = 0;
-	var temp_num_points = 0;
-	for (var k = 0; k < num_blocks; k++) {
-		temp_num_points = points_in_block[k];
-		if (temp_num_points > 0) {
+	for (var i = 0, i3 = 0; i < count; i++) {
+		var x = positionArray[i3 + 0] / 10;
+		var y = positionArray[i3 + 1] / 10;
+		var z = positionArray[i3 + 2] / 10;
+		var k = parentBlock[i];
 
-			var x_start = view.data[k]['xPlot'];
-			var y_start = view.data[k]['yPlot'];
-			var z_start = view.data[k]['zPlot'];
-			var x_end = x_start + 1;
-			var y_end = y_start + 1;
-			var z_end = z_start + 1;
+		var color = lut.getColor(view.data[k][options.propertyOfInterest]);
 
-			for (var j = 0; j < temp_num_points; j++) {
+		colors[i3 + 0] = color.r;
+		colors[i3 + 1] = color.g;
+		colors[i3 + 2] = color.b;
 
-				var color = lut.getColor(view.data[k][options.propertyOfInterest]);
-
-				colors[i3 + 0] = color.r;
-				colors[i3 + 1] = color.g;
-				colors[i3 + 2] = color.b;
-
-				if (x_start >= options.x_low && x_end <= options.x_high && y_start >= options.y_low && y_end <= options.y_high && z_start >= options.z_low && z_end <= options.z_high && view.data[k].selected) {
-					alphas[i] = options.pointCloudAlpha;
-					sizes[i] = options.pointCloudSize;
-				} else {
-					alphas[i] = 0;
-					sizes[i] = 0;
-				}
-
-				i++;
-				i3 += 3;
-			}
+		if (x >= options.x_low && x <= options.x_high && y >= options.y_low && y <= options.y_high && z >= options.z_low && z <= options.z_high && view.data[k].selected) {
+			alphas[i] = options.pointCloudAlpha;
+			sizes[i] = options.pointCloudSize;
+		} else {
+			alphas[i] = 0;
+			sizes[i] = 0;
 		}
+		i3 += 3;
 	}
 
 	view.System.geometry.addAttribute('customColor', new THREE.BufferAttribute(colors, 3));
--- 3DViews/PointCloud_selection.js
+++ 3DViews/PointCloud_selection.js
@@ -1,114 +1,118 @@
 export function getPointCloudGeometry(view){
 
-var uniforms = {
+	var uniforms = {
 
-	color:     { value: new THREE.Color( 0xffffff ) },
-	texture:   { value: new THREE.TextureLoader().load( "textures/sprites/disc.png" ) }
+		color:     { value: new THREE.Color( 0xffffff ) },
+		texture:   { value: new THREE.TextureLoader().load( "textures/sprites/disc.png" ) }
 
-};
+	};
 
-var shaderMaterial = new THREE.ShaderMaterial( {
+	var shaderMaterial = new THREE.ShaderMaterial( {
 
-	uniforms:       uniforms,
-	vertexShader:   document.getElementById( 'vertexshader' ).textContent,
-	fragmentShader: document.getElementById( 'fragmentshader' ).textContent,
+		uniforms:       uniforms,
+		vertexShader:   document.getElementById( 'vertexshader' ).textContent,
+		fragmentShader: document.getElementById( 'fragmentshader' ).textContent,
 
-	blending:       THREE.AdditiveBlending,
-	depthTest:      false,
-	transparent:    true
+		blending:       THREE.AdditiveBlending,
+		depthTest:      false,
+		transparent:    true
 
-});
+	});
 
-var options = view.options;
-var scene = view.scene;
+	var options = view.options;
+	var scene = view.scene;
 
 
-var particles = options.pointCloudParticles;
-var num_blocks = view.data.length;
-var points_in_block = new Float32Array(num_blocks);
-var total = 100;
-var count = 0;
+	var particles = options.pointCloudParticles;
+	var num_blocks = view.data.length;
+	var points_in_block = new Float32Array(num_blocks);
+	var total = 100;
+	var count = 0;
 
-for ( var k = 0; k < num_blocks; k ++) {
-	var num_points  = Math.min(Math.floor((view.data[k][options.density] / total) * particles),40);
-	points_in_block[k] = num_points;
-	count += num_points;
-}
-console.log("total points in cloud: ", count)
+	for ( var k = 0; k < num_blocks; k ++) {
+		var num_points  = Math.min(Math.floor((view.data[k][options.density] / total) * particles),40);
+		points_in_block[k] = num_points;
+		count += num_points;
+	}
+	console.log("total points in cloud: ", count)
 
-var geometry = new THREE.BufferGeometry();
+	var geometry = new THREE.BufferGeometry();
 
-var positions = new Float32Array(count*3);
-var colors = new Float32Array(count *3);
-var sizes = new Float32Array( count);
-var alphas = new Float32Array( count);
+	var positions = new Float32Array(count*3);
+	var colors = new Float32Array(count *3);
+	var sizes = new Float32Array( count);
+	var alphas = new Float32Array( count);
+	var parentBlock = new Float32Array( count);
 
-var colorMap = options.colorMap;
-var numberOfColors = 512;
+	var colorMap = options.colorMap;
+	var numberOfColors = 512;
 
-var lut = new THREE.Lut( colorMap, numberOfColors );
-lut.setMax( options.pointCloudColorSettingMax );
-lut.setMin( options.pointCloudColorSettingMin );
-view.lut = lut;
+	var lut = new THREE.Lut( colorMap, numberOfColors );
+	lut.setMax( options.pointCloudColorSettingMax );
+	lut.setMin( options.pointCloudColorSettingMin );
+	view.lut = lut;
 
-var i = 0, i3 = 0;
-var temp_num_points = 0;
-for ( var k = 0; k < num_blocks; k ++) {
-	temp_num_points  =  points_in_block[k];
-	if (temp_num_points > 0){
+	var i = 0, i3 = 0;
+	var temp_num_points = 0;
+	for ( var k = 0; k < num_blocks; k ++) {
+		temp_num_points  =  points_in_block[k];
+		if (temp_num_points > 0){
 
-		var x_start = view.data[k]['xPlot'];
-		var y_start = view.data[k]['yPlot'];
-		var z_start = view.data[k]['zPlot'];
-		var x_end = x_start + 1;
-		var y_end = y_start + 1;
-		var z_end = z_start + 1;
-		
-		for (var j = 0; j < temp_num_points; j ++){
-
-			var x = Math.random()*1  + x_start;
-			var y = Math.random()*1  + y_start;
-			var z = Math.random()*1  + z_start;
+			var x_start = view.data[k]['xPlot'];
+			var y_start = view.data[k]['yPlot'];
+			var z_start = view.data[k]['zPlot'];
+			var x_end = x_start + 1;
+			var y_end = y_start + 1;
+			var z_end = z_start + 1;
 			
-			positions[ i3 + 0 ] = x*10;
-			positions[ i3 + 1 ] = y*10;
-			positions[ i3 + 2 ] = z*10;
+			for (var j = 0; j < temp_num_points; j ++){
 
-			var color = lut.getColor( view.data[k][options.propertyOfInterest] );
-			
-			colors[ i3 + 0 ] = color.r;
-			colors[ i3 + 1 ] = color.g;
-			colors[ i3 + 2 ] = color.b;
-			
-			if (	(x_start >= options.x_low) 	&& (x_end <= options.x_high) 	&&
-				(y_start >= options.y_low) 	&& (y_end <= options.y_high)	&&
-				(z_start >= options.z_low) 	&& (z_end <= options.z_high)	&& view.data[k].selected)
-				{
-					alphas[ i ] = options.pointCloudAlpha;
-					sizes[ i ] = options.pointCloudSize;
+				var x = Math.random()*1  + x_start;
+				var y = Math.random()*1  + y_start;
+				var z = Math.random()*1  + z_start;
+				
+				positions[ i3 + 0 ] = x*10;
+				positions[ i3 + 1 ] = y*10;
+				positions[ i3 + 2 ] = z*10;
+
+				var color = lut.getColor( view.data[k][options.propertyOfInterest] );
+				
+				colors[ i3 + 0 ] = color.r;
+				colors[ i3 + 1 ] = color.g;
+				colors[ i3 + 2 ] = color.b;
+				
+				if (	(x_start >= options.x_low) 	&& (x_end <= options.x_high) 	&&
+					(y_start >= options.y_low) 	&& (y_end <= options.y_high)	&&
+					(z_start >= options.z_low) 	&& (z_end <= options.z_high)	&& view.data[k].selected)
+					{
+						alphas[ i ] = options.pointCloudAlpha;
+						sizes[ i ] = options.pointCloudSize;
+					}
+				else {
+					alphas[ i ] = 0;
+					sizes[ i ] = 0;
 				}
-			else {
-				alphas[ i ] = 0;
-				sizes[ i ] = 0;
+
+				parentBlock[i] = k;
+
+				
+				i++;
+				i3+=3;
 			}
-
-			
-			i++;
-			i3+=3;
-		}
-	}			
-}
-	
+		}			
+	}
+		
 
 
-geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
-geometry.addAttribute( 'customColor', new THREE.BufferAttribute( colors, 3 ) );
-geometry.addAttribute( 'size', new THREE.BufferAttribute( sizes, 1 ) );
-geometry.addAttribute( 'alpha', new THREE.BufferAttribute( alphas, 1 ) );
+	geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
+	geometry.addAttribute( 'customColor', new THREE.BufferAttribute( colors, 3 ) );
+	geometry.addAttribute( 'size', new THREE.BufferAttribute( sizes, 1 ) );
+	geometry.addAttribute( 'alpha', new THREE.BufferAttribute( alphas, 1 ) );
+	geometry.parentBlockMap = parentBlock;
 
-var System = new THREE.Points( geometry, shaderMaterial );
-view.System = System;
-scene.add( System );
+	var System = new THREE.Points( geometry, shaderMaterial );
+	view.System = System;
+	scene.add( System );
 
 }
 
@@ -117,79 +121,60 @@ scene.add( System );
 
 export function updatePointCloudGeometry(view){
 
-var options = view.options;
-
-var particles = options.pointCloudParticles;
-var num_blocks = view.data.length;
-var points_in_block = new Float32Array(num_blocks);
-var total = 100;
-var count = 0;
-
-for ( var k = 0; k < num_blocks; k ++) {
-	//var num_points  = Math.floor((unfilteredData[k]['n'] / total) * particles);
-	var num_points  = Math.min(Math.floor((view.data[k][options.density] / total) * particles),40);
-	points_in_block[k] = num_points;
-	count += num_points;
-}
+	var options = view.options;
+	var positionArray = view.System.geometry.attributes.position.array;
+	var parentBlock = view.System.geometry.parentBlockMap;
 
 
-var colors = new Float32Array(count *3);
-var sizes = new Float32Array( count);
-var alphas = new Float32Array( count);
+	var particles = options.pointCloudParticles;
+	var num_blocks = view.data.length;
+	var points_in_block = new Float32Array(num_blocks);
+	var count = view.System.geometry.attributes.size.array.length;
 
-var colorMap = options.colorMap;
-var numberOfColors = 512;
 
-var lut = new THREE.Lut( colorMap, numberOfColors );
-lut.setMax( options.pointCloudColorSettingMax );
-lut.setMin( options.pointCloudColorSettingMin );
-view.lut = lut;
+	var colors = new Float32Array(count *3);
+	var sizes = new Float32Array( count);
+	var alphas = new Float32Array( count);
 
-var i = 0, i3 = 0;
-var temp_num_points = 0;
-for ( var k = 0; k < num_blocks; k ++) {
-	temp_num_points  =  points_in_block[k];
-	if (temp_num_points > 0){
+	var colorMap = options.colorMap;
+	var numberOfColors = 512;
 
-		var x_start = view.data[k]['xPlot'];
-		var y_start = view.data[k]['yPlot'];
-		var z_start = view.data[k]['zPlot'];
-		var x_end = x_start + 1;
-		var y_end = y_start + 1;
-		var z_end = z_start + 1;
+	var lut = new THREE.Lut( colorMap, numberOfColors );
+	lut.setMax( options.pointCloudColorSettingMax );
+	lut.setMin( options.pointCloudColorSettingMin );
+	view.lut = lut;
+
+	for (var i = 0, i3 = 0; i < count; i++){
+		var x = positionArray[ i3 + 0 ]/10;
+		var y = positionArray[ i3 + 1 ]/10;
+		var z = positionArray[ i3 + 2 ]/10;
+		var k = parentBlock[i];
+
+		var color = lut.getColor( view.data[k][options.propertyOfInterest] );
+				
+		colors[ i3 + 0 ] = color.r;
+		colors[ i3 + 1 ] = color.g;
+		colors[ i3 + 2 ] = color.b;
 		
-		for (var j = 0; j < temp_num_points; j ++){
-
-			var color = lut.getColor( view.data[k][options.propertyOfInterest] );
-			
-			colors[ i3 + 0 ] = color.r;
-			colors[ i3 + 1 ] = color.g;
-			colors[ i3 + 2 ] = color.b;
-			
-			if (	(x_start >= options.x_low) 	&& (x_end <= options.x_high) 	&&
-				(y_start >= options.y_low) 	&& (y_end <= options.y_high)	&&
-				(z_start >= options.z_low) 	&& (z_end <= options.z_high)	&& view.data[k].selected)
-				{
-					alphas[ i ] = options.pointCloudAlpha;
-					sizes[ i ] = options.pointCloudSize;
-				}
-			else {
-				alphas[ i ] = 0;
-				sizes[ i ] = 0;
-			}
-
-			
-			i++;
-			i3+=3;
+		if (	(x >= options.x_low) 	&& (x <= options.x_high) 	&&
+				(y >= options.y_low) 	&& (y <= options.y_high)	&&
+				(z >= options.z_low) 	&& (z <= options.z_high)	&& 	view.data[k].selected)
+		{
+			alphas[ i ] = options.pointCloudAlpha;
+			sizes[ i ] = options.pointCloudSize;
 		}
-	}			
-}
+		else {
+			alphas[ i ] = 0;
+			sizes[ i ] = 0;
+		}
+		i3 += 3;
 
-view.System.geometry.addAttribute( 'customColor', new THREE.BufferAttribute( colors, 3 ) );
-view.System.geometry.addAttribute( 'size', new THREE.BufferAttribute( sizes, 1 ) );
-view.System.geometry.addAttribute( 'alpha', new THREE.BufferAttribute( alphas, 1 ) );
-}
+	}
 
+	view.System.geometry.addAttribute( 'customColor', new THREE.BufferAttribute( colors, 3 ) );
+	view.System.geometry.addAttribute( 'size', new THREE.BufferAttribute( sizes, 1 ) );
+	view.System.geometry.addAttribute( 'alpha', new THREE.BufferAttribute( alphas, 1 ) );
+}
 
 
 
