2f69fe1ac8a7ffaa3429ee836cbe71d11d5dc142
ray38 <xlei38@gatech.edu>
2019-03-11 15:56:55 -0400

    updated bond wire view

--- 2D3D_connection_heatmap_test.js
+++ 2D3D_connection_heatmap_test.js
@@ -46,29 +46,30 @@ var _MultiviewControlColorLegendJs = require("./MultiviewControl/colorLegend.js"
 
 var _UtilitiesScaleJs = require("./Utilities/scale.js");
 
+/*
 var opts = {
-	lines: 13, // The number of lines to draw
-	length: 38, // The length of each line
-	width: 17, // The line thickness
-	radius: 45, // The radius of the inner circle
-	scale: 1, // Scales overall size of the spinner
-	corners: 1, // Corner roundness (0..1)
-	color: '#ffffff', // CSS color or array of colors
-	fadeColor: 'transparent', // CSS color or array of colors
-	speed: 1, // Rounds per second
-	rotate: 0, // The rotation offset
-	animation: 'spinner-line-fade-quick', // The CSS animation name for the lines
-	direction: 1, // 1: clockwise, -1: counterclockwise
-	zIndex: 2e9, // The z-index (defaults to 2000000000)
-	className: 'spinner', // The CSS class to assign to the spinner
-	top: '50%', // Top position relative to parent
-	left: '50%', // Left position relative to parent
-	shadow: '0 0 1px transparent', // Box-shadow for the lines
-	position: 'absolute' // Element positioning
-};
+		lines: 13, // The number of lines to draw
+		length: 38, // The length of each line
+		width: 17, // The line thickness
+		radius: 45, // The radius of the inner circle
+		scale: 1, // Scales overall size of the spinner
+		corners: 1, // Corner roundness (0..1)
+		color: '#ffffff', // CSS color or array of colors
+		fadeColor: 'transparent', // CSS color or array of colors
+		speed: 1, // Rounds per second
+		rotate: 0, // The rotation offset
+		animation: 'spinner-line-fade-quick', // The CSS animation name for the lines
+		direction: 1, // 1: clockwise, -1: counterclockwise
+		zIndex: 2e9, // The z-index (defaults to 2000000000)
+		className: 'spinner', // The CSS class to assign to the spinner
+		top: '50%', // Top position relative to parent
+		left: '50%', // Left position relative to parent
+		shadow: '0 0 1px transparent', // Box-shadow for the lines
+		position: 'absolute' // Element positioning
+	};
 
 var spinnerContainer = document.getElementById("spinner");
-var spinner = new Spinner(opts).spin(spinnerContainer);
+var spinner = new Spinner(opts).spin(spinnerContainer);*/
 
 if (typeof data !== 'undefined') {
 	console.log(data);
@@ -171,8 +172,8 @@ function main(views, plotSetup) {
 	queue.awaitAll(function (error) {
 		if (error) throw error;
 		init();
-		spinner.stop();
-		spinnerContainer.parentNode.removeChild(spinnerContainer);
+		//spinner.stop();
+		//spinnerContainer.parentNode.removeChild(spinnerContainer);
 		animate();
 	});
 
