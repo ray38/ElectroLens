d33efe0bd40b9e49161027222da043c2151b3bf8
Drew Skwiers-Koballa <drew.koballa@gmail.com>
2020-10-23 20:44:18 -0700

    adds error checking to bootstrap progress bar

--- Utilities/bootstrapProgressBar.js
+++ Utilities/bootstrapProgressBar.js
@@ -3,7 +3,9 @@ export function queueUpdateProgressBar() {
     let currentWidth = parseInt(document.getElementById("loading-progress").style["width"]);
     let barQueueLength = parseInt(document.getElementById("loading-progress").getAttribute("queueLength"));
     let newWidth = (currentWidth + barQueueLength).toString();
-    document.getElementById("loading-progress").style["width"] = newWidth+"%";
+    if (newWidth && newWidth <= 100.0 && document.getElementById("loading-progress")) {
+        document.getElementById("loading-progress").style["width"] = newWidth+"%";
+    }
 }
 
 export function streamUpdateProgressBar(chunkSize, totalSize, filename) {
@@ -12,14 +14,20 @@ export function streamUpdateProgressBar(chunkSize, totalSize, filename) {
     let forThisQueueItem = ( barQueueLength * ( 1.0 / totalChunks ) );
     let currentWidth = parseInt(document.getElementById("loading-progress").style["width"]);
     let newWidth = (currentWidth + forThisQueueItem).toString();
-    document.getElementById("loading-progress").style["width"] = newWidth+"%";
+    if (newWidth && newWidth <= 100.0 && document.getElementById("loading-progress")) {
+        document.getElementById("loading-progress").style["width"] = newWidth+"%";
+    }
     setProgressMessage("Loading " + filename);
 }
 
 export function clearProgressMessage() {
-    document.getElementById("loading-message").innerHTML = "";
+    if (document.getElementById("loading-progress")) {
+        document.getElementById("loading-message").innerHTML = "";
+    }
 }
 
 export function setProgressMessage(newMessage) {
-    document.getElementById("loading-message").innerHTML = "<h5>"+newMessage+"</h5>";
+    if (document.getElementById("loading-message")) {
+        document.getElementById("loading-message").innerHTML = "<h5>"+newMessage+"</h5>";
+    }
 }
